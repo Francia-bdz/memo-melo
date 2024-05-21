474ddf24c60ef98edddb3e87e8fd2dc8b0ee0eee
Francia-bdz <francia.bidzimou@mmibordeaux.com>
2024-05-21 22:51:46 +0200

    change : css to scss in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,71 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Mémo-mélo</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
 
-    <style>
-        h1 {
-            text-shadow: -2px 2px 0 #000,
-                2px 2px 0 #000,
-                2px -2px 0 #000,
-                -2px -2px 0 #000;
-        }
-    </style>
 
 </head>
 
-<body class="bg-beige ">
-    <div class="bg-beige h-full w-full">
-        <div class="flex flex-col items-center">
-            <div class="bg-green w-max rounded-md border-2 border-black flex mt-24 pb-10">
-                <div class="flex flex-col mt-16 ml-20 mr-28">
-                    <h1 class="font-alinsa text-9xl	text-beige ">MÉMO</br>MÉLO</h1>
-                    <h2 class="text-beige text-2xl	"> Gardez le rythme de </br>votre progression </h2>
-                </div>
-                <img src="{{ asset('assets/images/memo-melo.svg') }}" class="max-w-xs mr-10">
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="content">
+                <h1>MÉMO<br>MÉLO</h1>
+                <h2>Gardez le rythme de<br>votre progression</h2>
             </div>
-            <div class="flex mt-10 gap-10	">
-                @auth
-        
-                    <form action="{{ route('logout') }}" method="POST">
-                        @csrf
-                        <button type="submit" class="bg-pink rounded-full border border-black transition shadow-button hover:bg-pinkHover hover:shadow-buttonHover p-1">
-                            <span class="font-inter px-3 py-2 text-black focus:outline-none">
-                                SE DÉCONNECTER
-                            </span>
-                        </button>
-                    </form>
+            <img src="{{ asset('assets/images/memo-melo.svg') }}" class="image">
+        </div>
+        <div class="buttons">
+            @auth
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="button button-pink">SE DÉCONNECTER</button>
+            </form>
+            @else
+            <form action="{{ route('demo-login') }}" method="POST">
+                @csrf
+                <button type="submit" class="button button-pink">VOIR LA DÉMO</button>
+            </form>
+            @endauth
 
-                @else
-                    <form action="{{ route('demo-login') }}" method="POST">
-                        @csrf
-                        <button type="submit" class="bg-pink rounded-full border border-black transition shadow-button hover:bg-pinkHover hover:shadow-buttonHover p-1">
-                            <span class="font-inter px-3 py-2 text-black focus:outline-none">
-                                VOIR LA DÉMO
-                            </span>
-                        </button>
-                    </form>
-
-                @endauth
-
-                @auth
-                    <button
-                        class="bg-green rounded-full border border-black transition shadow-button hover:bg-greenHover hover:shadow-buttonHover p-1">
-                        <a href="{{ url('/songs') }}" class="font-inter px-3 py-2 text-beige transition focus:outline-none">
-                            ACCÉDER AU RÉPERTOIRE
-                        </a>
-                    </button>
-                @else
-                    <button
-                        class="bg-green rounded-full border border-black transition shadow-button hover:bg-greenHover hover:shadow-buttonHover p-1">
-                        <a href="{{ route('login') }}"
-                            class="font-inter px-3 py-2 text-beige transition focus:outline-none">
-                            SE CONNECTER
-                        </a>
-                    </button>
-
-                @endauth
-            </div>
+            @auth
+            <a href="{{ url('/songs') }}" class="button button-green">ACCÉDER AU RÉPERTOIRE</a>
+            @else
+            <a href="{{ route('login') }}" class="button button-green">SE CONNECTER</a>
+            @endauth
         </div>
     </div>
 </body>
